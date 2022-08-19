cefda739bc2936384191de7b5a079f116494fe90
muffinweb <ugurcengiztr35@gmail.com>
2022-08-19 23:10:40 +0300

    We've done error, bad thing happened errors, now its time to pass test

--- customTests/manuel-test.php
+++ customTests/manuel-test.php
@@ -1,4 +1,10 @@
 <?php
 
-echo "Bu bir diger hata belirtme sekili olarak degisti";
-die(43);
+$is_there_error = false;
+
+if($is_there_error){
+    echo "Bu bir diger hata belirtme sekili olarak degisti";
+    die(43);
+}
+
+fwrite(STDOUT, "\033[32mMANUEL TEST GECTI \033[0m\n");
